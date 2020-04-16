5b41f93f2769c58c9d79ee735c3e51ebf1b4a424
YoungHo Choi <0505zxc@gmail.com>
2020-04-16 11:32:14 +0900

    레일즈 초기화

--- app/models/todo.rb
+++
@@ -1,3 +0,0 @@
-class Todo < ApplicationRecord
-    has_many :tasks
-end
--- db/migrate/20200403082030_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[6.0]
-  def change
-    create_table :todos do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
